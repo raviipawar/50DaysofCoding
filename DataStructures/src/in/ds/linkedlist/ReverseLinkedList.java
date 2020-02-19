ccf8a7273bcee080a7e1dcc86315e3f38d3d24d8
ravindra <ravibalajipawar@gmail.com>
2020-02-19 21:04:15 +0530

    LinkedList Basic Examples

---
+++ DataStructures/src/in/ds/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,32 @@
+package in.ds.linkedlist;
+
+import java.util.LinkedList;
+
+/**
+ * @author Ravindra #50DaysofCoding
+ * Reversing a LinkedList
+ */
+public class ReverseLinkedList {
+
+	public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("ravin");
+        linkedList.add("shana");
+        linkedList.add("venki");
+        linkedList.add("raghu");
+        System.out.println("Before Reversing :"+ linkedList);
+
+        System.out.println("After Reversing :"+reverseLinkedList(linkedList));
+
+    }
+
+    public static LinkedList<String> reverseLinkedList(LinkedList<String> linkedList) {
+        LinkedList<String> reverseLinkedList = new LinkedList<>();
+        for(int i = linkedList.size() -1; i >= 0 ;--i){
+            reverseLinkedList.add(linkedList.get(i));
+        }
+        return reverseLinkedList;
+    }
+}
+//Time Complexity : O(n)
+//Space Complexity : O(n)
---
+++ DataStructures/src/in/ds/linkedlist/ReverseLinkedList2.java
@@ -0,0 +1,40 @@
+package in.ds.linkedlist;
+
+import java.util.LinkedList;
+
+/**
+ * @author Ravindra #50DaysofCoding
+ * Reversing a LinkedList without using another Linkedlist
+ */
+public class ReverseLinkedList2 {
+
+	public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("ravin");
+        linkedList.add("shana");
+        linkedList.add("venki");
+        linkedList.add("raghu");
+        linkedList.add("ramesh");
+        linkedList.add("lucky");
+        linkedList.add("tarun");
+
+        System.out.println("Before Reversing :"+ linkedList);
+
+        System.out.println("After Reversing :"+reverseLinkedList(linkedList));
+
+    }
+
+    private static LinkedList <String> reverseLinkedList(LinkedList<String> linkedList) {
+
+        for(int i = 0; i < linkedList.size() / 2 ; i++) {
+            String temp = linkedList.get(i);
+            linkedList.set(i, linkedList.get(linkedList.size()-i-1));
+            linkedList.set(linkedList.size()-i-1,temp);
+        }
+        return linkedList;
+    }
+}
+
+//Time Complexity: O(n/2)
+//Space Complexity: O(1)
+
